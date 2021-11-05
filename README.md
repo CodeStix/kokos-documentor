02e9dddd06842c4f03d98b9346f670d22f641ca9
Stijn Rogiest <reddusted@gmail.com>
2021-11-05 11:26:25 +0100

    Create README & read page count

---
+++ README.md
@@ -0,0 +1,7 @@
+# kokos-documentor
+
+The web application to document the kokos project.
+
+## Notes
+
+- The `public/pdf.worker.js` is copied from `node_modules/pdfjs-dist/build`
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as pdf from "pdfjs-dist";
+
+pdf.GlobalWorkerOptions.workerSrc = "/pdf.worker.js";
 
 export default function App() {
-    return <div className="text-blue-400">test</div>;
+    const [pageCount, setPageCount] = useState(0);
+
+    async function loadPdf() {
+        // https://mozilla.github.io/pdf.js/examples/
+        let res = await pdf.getDocument("/multiboot.pdf").promise;
+
+        setPageCount(res.numPages);
+    }
+
+    useEffect(() => {
+        loadPdf();
+    }, []);
+
+    return (
+        <div className="text-blue-400">
+            <pre>page count = {pageCount}</pre>
+        </div>
+    );
 }
